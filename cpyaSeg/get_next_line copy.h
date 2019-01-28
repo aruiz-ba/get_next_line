6746f6f59a486db3066673164a3f1d41973eb02e
Adriano Ruiz barbero <aruiz-ba@e1z1r3p36.42.us.org>
2019-01-28 09:53:24 -0800

    hola

---
+++ cpyaSeg/get_next_line copy.h
@@ -0,0 +1,11 @@
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# define BUFF_SIZE 16
+# define FD_SIZE 255
+
+
+#include "./libft/includes/libft.h"
+int get_next_line(const int fd, char **line);
+
+#endif
